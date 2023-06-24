fdc7ef54e6d70ac9208263a19ef75ac0ef5d96da
goodjonte <goodjonte@gmail.com>
2023-06-24 14:27:27 +1200

    Cleaned up user profile structure

--- src/Pages/UserProfile.js
+++ src/Pages/UserProfile.js
@@ -5,49 +5,46 @@ import * as ApiOperations from '../Operations/ApiOperations';
 export default function UserProfile() {
 
     const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
     
-
+    //Get user id from url and get user data from database upon first load
     useEffect(() => {
         const urlParams = new URLSearchParams(window.location.search);
         const userId = urlParams.get('id');
 
         ApiOperations.Get('User/GetUser?id=' + userId).then((response) => {
             setUser(response);
+            setLoading(false);
         });
-
-        
     }, []);
 
-    function profileHtml(userObject){
-        if(userObject.usersRole === 0 || userObject.usersRole ===  1){
 
+    //If pages is loading, display loading message
+    if(loading){
+        return(
+            <div>
+                <h1>Loading...</h1>
+            </div>
+        );
+    }else{  //Else display users profile based on users role 
+    
+        if(user.usersRole === 0 || user.usersRole ===  1){
             return(
                 <div>
-                    <h1>{userObject.firstName} {userObject.lastName}</h1>
-                    <h2>Email:{userObject.email}</h2>
+                    <h1>{user.firstName} {user.lastName}</h1>
+                    <h2>Email:{user.email}</h2>
                 </div>
             );
         }else{
             return(
                 <div>
-                    <h1>{userObject.firstName} {userObject.lastName}</h1>
-                    <h2>Parents Name:{userObject.parentName}</h2>
-                    <h2>Parents Number:{userObject.parentPhone}</h2>
+                    <h1>{user.firstName} {user.lastName}</h1>
+                    <h2>Parents Name:{user.parentName}</h2>
+                    <h2>Parents Number:{user.parentPhone}</h2>
                 </div>
             );
         }
-    }
-   
-    //Make into form with drop down box next to each student with absent, present, unjustified, justified
-  return (
-    <div className="App">
-        {
-            user != null ? 
-            profileHtml(user)
-            : null
-        }
-    </div>
-  );
-}
+    };
+};
 
 
